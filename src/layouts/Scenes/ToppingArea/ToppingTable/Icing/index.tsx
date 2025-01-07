7e8c67a8764a18be7ca7fcb746209e9eb1460ef7
Diky-Without-C <snayzher@gmail.com>
2025-01-07 21:19:13 +0700

    feat: add animation for icing when its dragging

--- src/layouts/Scenes/ToppingArea/ToppingTable/Icing/index.tsx
+++ src/layouts/Scenes/ToppingArea/ToppingTable/Icing/index.tsx
@@ -1,22 +1,28 @@
+import { useState } from "react";
 import { ICING_VARIANT } from "@constant/Donuts/donuts-detail.json";
 import Draggable from "@components/Draggable";
 import { icingImages } from "@assets/toppingArea/config";
 import Overlay from "@components/Overlay";
 
 export default function Icing() {
+  const [selected, setSelected] = useState<number | null>(null);
+
   return (
     <div className="relative -top-20 flex h-1/2 w-full items-center justify-around">
-      {ICING_VARIANT.map((icing, index) => {
-        return (
-          <Draggable
-            key={icing.id}
-            id={`icing-${index}`}
-            className="flex h-32 w-16 items-center justify-center transition-all duration-500 will-change-transform"
-          >
-            <Overlay src={icingImages[icing.id as keyof typeof icingImages]} />
-          </Draggable>
-        );
-      })}
+      {ICING_VARIANT.map((icing, index) => (
+        <Draggable
+          key={icing.id}
+          id={`icing-${index}`}
+          onDraggingStart={() => setSelected(index)}
+          onDraggingEnd={() => setSelected(null)}
+          className="relative flex h-32 w-16 items-center justify-center transition-all duration-500"
+        >
+          <Overlay
+            src={icingImages[icing.id as keyof typeof icingImages]}
+            className={selected === index ? "rotate-180 transition-all" : ""}
+          />
+        </Draggable>
+      ))}
     </div>
   );
 }
