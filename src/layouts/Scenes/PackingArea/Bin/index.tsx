ef7669f279c33fa71d5dd4e6e16d3b2f3e78aab2
Diky-Without-C <snayzher@gmail.com>
2025-01-08 16:06:46 +0700

    style: adjust the trash can overlay size

--- src/layouts/Scenes/PackingArea/Bin/index.tsx
+++ src/layouts/Scenes/PackingArea/Bin/index.tsx
@@ -5,7 +5,7 @@ import { TrashCanImage } from "@assets/PackingArea/config";
 export default function Bin() {
   return (
     <Droppable id="trash-can-3" className="relative h-1/5 w-2/5">
-      <Overlay src={TrashCanImage} />
+      <Overlay src={TrashCanImage} className="h-full w-full" />
     </Droppable>
   );
 }
