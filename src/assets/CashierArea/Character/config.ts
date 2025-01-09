71fc6e49712b1a82cdfaba1dbeef8991427367f7
Diky-Without-C <snayzher@gmail.com>
2025-01-09 17:56:02 +0700

    fix: import issue

--- src/assets/CashierArea/Character/config.ts
+++ src/assets/CashierArea/Character/config.ts
@@ -5,8 +5,8 @@ import Damar from "./Damar.png";
 import Dina from "./Dina.png";
 import Doni from "./Doni.png";
 import Gea from "./Gea.png";
-import Jihan from "./Jihan.png";
-import Sania from "./Sania.png";
+import Jihan from "./jihan.png";
+import Sania from "./sania.png";
 import Tania from "./Tania.png";
 
 const CharacterImages = {
