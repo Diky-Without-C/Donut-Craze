a2063ccf24d5d310aeebec98e93bc43ab949cc49
Diky-Without-C <snayzher@gmail.com>
2025-01-10 18:39:39 +0700

    feat: add start button

--- src/layouts/Scenes/CashierArea/index.tsx
+++ src/layouts/Scenes/CashierArea/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useCustomersStore from "@services/stores/customersStore";
 import Cashier from "./Cashier";
 import Character from "./Character";
@@ -6,6 +7,7 @@ import Dialog from "./Character/Dialog";
 
 export default function CashierArea() {
   const { customers } = useCustomersStore();
+  const [isStart, setIsStart] = useState(false);
 
   return (
     <section className="flex h-full min-w-full snap-start flex-col justify-end">
@@ -14,8 +16,23 @@ export default function CashierArea() {
           <Cashier />
         </div>
         <div className="relative flex h-full w-9/12 items-end">
-          {customers[0] && <Character character={customers[0]} />}
-          {customers[0] && <Dialog character={customers[0]} />}
+          {!isStart ? (
+            <div className="flex h-full w-full items-center justify-center bg-slate-200">
+              <button
+                onClick={() => setIsStart(true)}
+                className="flex h-16 w-36 -translate-x-full cursor-pointer items-center justify-center rounded-full border-4 border-red-400 bg-white px-4 py-3 font-mono text-2xl font-bold text-red-400"
+              >
+                Start
+              </button>
+            </div>
+          ) : (
+            customers[0] && (
+              <>
+                <Character character={customers[0]} />
+                <Dialog character={customers[0]} />
+              </>
+            )
+          )}
         </div>
       </div>
       <Table />
