6ab5716e51eaf08d49391db84dd68579fd15c951
Diky-Without-C <snayzher@gmail.com>
2025-01-13 18:17:39 +0700

    feat: use level as current day

--- src/layouts/Home/Level/index.tsx
+++ src/layouts/Home/Level/index.tsx
@@ -1,3 +1,8 @@
+import useLocalStorage from "@hooks/useLocalStorage";
+
 export default function Level() {
-  return <h2 className="text-2xl font-semibold text-gray-800">Hari ke 1</h2>;
+  const [level] = useLocalStorage("donut-craze-level", 1);
+  return (
+    <h2 className="text-2xl font-semibold text-gray-800">Hari ke {level}</h2>
+  );
 }
