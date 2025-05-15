6edd9c48daea012795b2b55fbd5ec1a4ad373142
Diky-Without-C <snayzher@gmail.com>
2025-05-15 22:43:17 +0700

    feat: display name the character

--- src/layouts/Scenes/CashierArea/Customer/Character/index.tsx
+++ src/layouts/Scenes/CashierArea/Customer/Character/index.tsx
@@ -1,6 +1,7 @@
 import { ComponentProps } from "react";
 import Droppable from "@components/Droppable";
 import Overlay from "@components/Overlay";
+import useCustomersStore from "@services/stores/customersStore";
 
 interface CharacterProps extends ComponentProps<"div"> {
   image: string;
@@ -11,12 +12,17 @@ export default function Character({
   className,
   ...props
 }: CharacterProps) {
+  const { customers } = useCustomersStore();
+
   return (
     <Droppable
       {...props}
       id="customer"
       className={`${className} relative flex h-3/4 w-3/12 items-end justify-center`}
     >
+      <span className="absolute top-0 z-10 h-7 -translate-y-20 rounded bg-white/60 px-4 font-mono text-lg font-bold tracking-wide">
+        {customers[0].name}
+      </span>
       <Overlay src={image} className="min-w-[150%]" />
     </Droppable>
   );
