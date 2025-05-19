b63c7755dde3431f5cb2aece2d484dddbb61b6be
Diky-Without-C <snayzher@gmail.com>
2025-05-19 08:16:10 +0700

    feat: donut can be moved directly after being molded

--- src/layouts/Scenes/FriyingArea/Table/Dough/Hints.tsx
+++ src/layouts/Scenes/FriyingArea/Table/Dough/Hints.tsx
@@ -15,8 +15,13 @@ export default function Hints({ grid, isDelay }: HintsProps) {
         const opacity =
           (isVisible || isMolded) && !isDelay ? "opacity-100" : "opacity-0";
         const border = isMolded ? "border-opacity-0" : "";
+        const areAllMolded = grid.every((state) => state.isMolded);
 
-        return (
+        return isMolded ? (
+          <div
+            className={`${areAllMolded ? "before:bg-transparent" : "before:bg-[#ffc798]"} relative flex size-24 scale-90 items-center justify-center rounded-full bg-table-primary before:absolute before:size-8 before:rounded-full before:content-['']`}
+          ></div>
+        ) : (
           <Droppable
             key={index}
             id={`dough-${index}`}
--- src/layouts/Scenes/FriyingArea/Table/Dough/ShapedDough.tsx
+++ src/layouts/Scenes/FriyingArea/Table/Dough/ShapedDough.tsx
@@ -1,30 +1,15 @@
-import {
-  useDoughStore,
-  useShapedDoughStore,
-} from "@services/stores/fryingAreaStore";
+import { useShapedDoughStore } from "@services/stores/fryingAreaStore";
 import Donut from "@components/Donut";
 
 export default function ShapedDough() {
-  const { dough } = useDoughStore();
   const { shapedDough } = useShapedDoughStore();
-  const areAllMolded = dough.grid.every((state) => state.isMolded);
 
   return (
     <div className="absolute flex h-full w-full flex-wrap justify-center gap-x-4 gap-y-2">
       {shapedDough.map((donut, index) => {
         return (
           <div key={index} className="flex size-24 items-center justify-center">
-            {donut && (
-              <Donut
-                donut={donut}
-                condition={areAllMolded}
-                className={
-                  dough.grid[index].isMolded && !areAllMolded
-                    ? "pointer-events-none"
-                    : ""
-                }
-              />
-            )}
+            {donut && <Donut donut={donut} />}
           </div>
         );
       })}
